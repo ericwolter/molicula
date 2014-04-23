76d1e93c6a0f5f3be3f4514645d46e2d1f47e384
Eric Wolter <eric.wolter@gmx.de>
2014-04-23 11:31:45 +0200

    stuff

--- moliculaTests/moliculaTests.h
+++ moliculaTests/moliculaTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Eric Wolter. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface moliculaTests : SenTestCase
+@interface moliculaTests : XCTestCase
 
 @end
